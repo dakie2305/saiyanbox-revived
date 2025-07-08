272f24de880fc7e2e11a9d315cf98ca1f362688c
satthukid000 <satthubongmoi2305@gmail.com>
2025-07-08 21:31:11 +0700

    update: add new status effects

--- Content/StatusEffects/CustomStatusEffects.cs
+++ Content/StatusEffects/CustomStatusEffects.cs
@@ -385,6 +385,302 @@ namespace SaiyanboxRevived.Content
             addToLocale(auraBlueSEffect.id, "Blue Spark Aura", "A refined divine aura crackling with power.");
             #endregion
 
+            #region JirenAura
+            var JirenAura = new StatusAsset()
+            {
+                id = "JirenAura",
+                render_priority = 5,
+                duration = 100f,
+                animated = true,
+                animation_speed = 0.15f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Advanced,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "JirenAura",
+                path_icon = "ui/icons/JirenAura"
+            };
+            JirenAura.locale_id = $"status_title_{JirenAura.id}";
+            JirenAura.locale_description = "status_description_JirenAura";
+            JirenAura.base_stats = new();
+            JirenAura.base_stats.set(CustomBaseStatsConstant.Scale, 0.0f);
+            JirenAura.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{JirenAura.texture}", false);
+            AssetManager.status.add(JirenAura);
+            addToLocale(JirenAura.id, "Jiren Aura", "Surrounded by intense pressure.");
+            #endregion
+
+            #region BrollyAura
+            var BrollyAura = new StatusAsset()
+            {
+                id = "BrollyAura",
+                render_priority = 5,
+                duration = 100f,
+                animated = true,
+                animation_speed = 0.15f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Advanced,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "BrollyAura",
+                path_icon = "ui/icons/BrollyAura"
+            };
+            BrollyAura.locale_id = $"status_title_{BrollyAura.id}";
+            BrollyAura.locale_description = "status_description_BrollyAura";
+            BrollyAura.base_stats = new();
+            BrollyAura.base_stats.set(CustomBaseStatsConstant.Scale, 0.0f);
+            BrollyAura.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{BrollyAura.texture}", false);
+            AssetManager.status.add(BrollyAura);
+            addToLocale(BrollyAura.id, "Brolly Aura", "Radiating berserker energy.");
+            #endregion
+
+            #region BrolySSJ4Aura
+            var BrolySSJ4Aura = new StatusAsset()
+            {
+                id = "BrolySSJ4Aura",
+                render_priority = 5,
+                duration = 100f,
+                animated = true,
+                animation_speed = 0.15f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Advanced,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "BrolySSJ4Aura",
+                path_icon = "ui/icons/BrolySSJ4Aura"
+            };
+            BrolySSJ4Aura.locale_id = $"status_title_{BrolySSJ4Aura.id}";
+            BrolySSJ4Aura.locale_description = "status_description_BrolySSJ4Aura";
+            BrolySSJ4Aura.base_stats = new();
+            BrolySSJ4Aura.base_stats.set(CustomBaseStatsConstant.Scale, 0.0f);
+            BrolySSJ4Aura.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{BrolySSJ4Aura.texture}", false);
+            AssetManager.status.add(BrolySSJ4Aura);
+            addToLocale(BrolySSJ4Aura.id, "Broly SSJ4 Aura", "Explosive primal energy erupts.");
+            #endregion
+
+            #region GokuAttack
+            var GokuAttack = new StatusAsset()
+            {
+                id = "GokuAttack",
+                render_priority = 5,
+                duration = 1f,
+                animated = true,
+                animation_speed = 0.1f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Basic,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "GokuAttack",
+                path_icon = "ui/icons/GokuAttack"
+            };
+            GokuAttack.locale_id = $"status_title_{GokuAttack.id}";
+            GokuAttack.locale_description = "status_description_GokuAttack";
+            GokuAttack.base_stats = new();
+            GokuAttack.base_stats.set(CustomBaseStatsConstant.Scale, 0.0f);
+            GokuAttack.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{GokuAttack.texture}", false);
+            AssetManager.status.add(GokuAttack);
+            addToLocale(GokuAttack.id, "Goku Attack", "Swift blow empowered by spirit.");
+            #endregion
+
+            #region GogetaAttack
+            var GogetaAttack = new StatusAsset()
+            {
+                id = "GogetaAttack",
+                render_priority = 5,
+                duration = 1f,
+                animated = true,
+                animation_speed = 0.1f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Basic,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "GogetaAttack",
+                path_icon = "ui/icons/GogetaAttack"
+            };
+            GogetaAttack.locale_id = $"status_title_{GogetaAttack.id}";
+            GogetaAttack.locale_description = "status_description_GogetaAttack";
+            GogetaAttack.base_stats = new();
+            GogetaAttack.base_stats.set(CustomBaseStatsConstant.Scale, 0.0f);
+            GogetaAttack.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{GogetaAttack.texture}", false);
+            AssetManager.status.add(GogetaAttack);
+            addToLocale(GogetaAttack.id, "Gogeta Attack", "Unyielding flurry of blows.");
+            #endregion
+
+            #region GokuDodge
+            var GokuDodge = new StatusAsset()
+            {
+                id = "GokuDodge",
+                render_priority = 5,
+                duration = 1f,
+                animated = true,
+                animation_speed = 0.1f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Basic,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "GokuDodge",
+                path_icon = "ui/icons/GokuDodge"
+            };
+            GokuDodge.locale_id = "status_title_GokuDodge";
+            GokuDodge.locale_description = $"status_description_{GokuDodge}";
+            GokuDodge.base_stats = new();
+            GokuDodge.base_stats.set(CustomBaseStatsConstant.Scale, 0.0f);
+            GokuDodge.base_stats.set(CustomBaseStatsConstant.Armor, 100f);
+            GokuDodge.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{GokuDodge.texture}", false);
+            AssetManager.status.add(GokuDodge);
+            addToLocale(GokuDodge.id, "Goku Dodge", "Ultra instinct reflex.");
+            #endregion
+
+            #region JirenAttack
+            var JirenAttack = new StatusAsset()
+            {
+                id = "JirenAttack",
+                render_priority = 5,
+                duration = 1f,
+                animated = true,
+                animation_speed = 0.1f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Basic,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "JirenAttack",
+                path_icon = "ui/icons/JirenAttack"
+            };
+            JirenAttack.locale_id = $"status_title_{JirenAttack.id}";
+            JirenAttack.locale_description = "status_description_JirenAttack";
+            JirenAttack.base_stats = new();
+            JirenAttack.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{JirenAttack.texture}", false);
+            AssetManager.status.add(JirenAttack);
+            addToLocale(JirenAttack.id, "Jiren Attack", "Immeasurable destructive force.");
+            #endregion
+
+            #region BrollyAttack
+            var BrollyAttack = new StatusAsset()
+            {
+                id = "BrollyAttack",
+                render_priority = 5,
+                duration = 1f,
+                animated = true,
+                animation_speed = 0.1f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Basic,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "BrollyAttack",
+                path_icon = "ui/icons/BrollyAttack"
+            };
+            BrollyAttack.locale_id = $"status_title_{BrollyAttack.id}";
+            BrollyAttack.locale_description = "status_description_BrollyAttack";
+            BrollyAttack.base_stats = new();
+            BrollyAttack.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{BrollyAttack.texture}", false);
+            AssetManager.status.add(BrollyAttack);
+            addToLocale(BrollyAttack.id, "Brolly Attack", "Savage brute force unleashed.");
+            #endregion
+
+            #region BrolySSJ4Attack
+            var BrolySSJ4Attack = new StatusAsset()
+            {
+                id = "BrolySSJ4Attack",
+                render_priority = 5,
+                duration = 1f,
+                animated = true,
+                animation_speed = 0.1f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Basic,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "BrolySSJ4Attack",
+                path_icon = "ui/icons/BrolySSJ4Attack"
+            };
+            BrolySSJ4Attack.locale_id = $"status_title_{BrolySSJ4Attack.id}";
+            BrolySSJ4Attack.locale_description = "status_description_BrolySSJ4Attack";
+            BrolySSJ4Attack.base_stats = new();
+            BrolySSJ4Attack.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{BrolySSJ4Attack.texture}", false);
+            AssetManager.status.add(BrolySSJ4Attack);
+            addToLocale(BrolySSJ4Attack.id, "Broly SSJ4 Attack", "Ferocious power strikes.");
+            #endregion
+
+            #region EscanorAttack
+            var EscanorAttack = new StatusAsset()
+            {
+                id = "EscanorAttack",
+                render_priority = 5,
+                duration = 1f,
+                animated = true,
+                animation_speed = 0.2f,
+                is_animated_in_pause = true,
+                can_be_flipped = true,
+                use_parent_rotation = false,
+                removed_on_damage = false,
+                cancel_actor_job = false,
+                need_visual_render = true,
+                scale = 1.0f,
+                tier = StatusTier.Basic,
+                material_id = "mat_world_object_lit",
+                material = material,
+                texture = "EscanorAttack",
+                path_icon = "ui/icons/EscanorAttack"
+            };
+            EscanorAttack.locale_id = $"status_title_{EscanorAttack.id}";
+            EscanorAttack.locale_description = "status_description_EscanorAttack";
+            EscanorAttack.base_stats = new();
+            EscanorAttack.sprite_list = SpriteTextureLoader.getSpriteList($"effects/{EscanorAttack.texture}", false);
+            AssetManager.status.add(EscanorAttack);
+            addToLocale(EscanorAttack.id, "Escanor Attack", "Power of the sun compressed.");
+            #endregion
 
         }
 
